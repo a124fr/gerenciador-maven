374e0c21b01b4b2abad44b9e41c5f63f154a127f
Francisco de Assis <a124fr@gmail.com>
2021-12-19 22:57:50 -0300

    Ordenando execução dos Filtros e criando controlador como um filtro

--- src/main/java/br/com/empresa/gerenciador/servlet/ControladorFilter.java
+++ src/main/java/br/com/empresa/gerenciador/servlet/ControladorFilter.java
@@ -1,22 +1,27 @@
 package br.com.empresa.gerenciador.servlet;
 
 import java.io.IOException;
-
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import br.com.empresa.gerenciador.acao.Acao;
 
-//@WebServlet("/entrada")
-public class ControladorServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+//@WebFilter("/entrada")
+public class ControladorFilter implements Filter {
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest request = (HttpServletRequest)servletRequest;
+		HttpServletResponse response = (HttpServletResponse)servletResponse;
 		
 		String paramAcao = request.getParameter("acao");
 		
@@ -38,8 +43,6 @@ public class ControladorServlet extends HttpServlet {
 			rd.forward(request, response);
 		} else {
 			response.sendRedirect(tipo[1]);
-		}		
-			
+		}
 	}
-
 }
